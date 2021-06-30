32f466740bef6d3dd0f3b99d3e07ee4ed424fda0
Mohebullah Mir <56857610+Mohebullah98@users.noreply.github.com>
2021-06-30 01:33:58 +0000

    Reduced the amount of switch cases by a lot by adding words like thousand or million after the ones column instead of hardcoding them. supports upto 999999999

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
-  public static void convert(int number){
-    String n = Integer.toString(number);
+  public static void converter(long number){
+    String n = Long.toString(number);
     int digit;
     char digitChar;
     String word ="";
@@ -8,11 +8,11 @@ class Main {
     for(int i=0;i<n.length();i++){//split number into digits to convert each digit to word
       digitChar=n.charAt(i);
       digit= Character.getNumericValue(digitChar);
-      if(n.length()-i==6){
+      if((n.length()-i)%3==0){
         switch(digit){
           case 0:word="";
           break;
-          case 1: word="one hundred";
+          case 1: word="One hundred";
           break;
           case 2: word="Two hundred";
           break;
@@ -31,8 +31,9 @@ class Main {
           case 9: word ="Nine Hundred";break;
         }
         System.out.print(word+" ");
-      }
-      if(n.length()-i==5){
+
+  }
+  if((n.length()-i)%3==2){
         switch(digit){
           case 0:word= "";
           break;
@@ -59,22 +60,51 @@ class Main {
         
         System.out.print(word+"");
       }
-      if(n.length()-i==4&&Teen==false){
+      if((n.length()-i)%3==1&&Teen==false){
         switch(digit){
-          case 0: word="Thousand";break;
-          case 1: word="One thousand";break;
-          case 2: word ="Two thousand";break;
-          case 3: word ="Three thousand";break;
-          case 4: word ="Four thousand";break;
-          case 5: word="Five thousand";break;
-          case 6:word="Six thousand";break;
-          case 7:word="Seven thousand";break;
-          case 8:word ="Eight thousand";break;
-          case 9:word="Nine thousand";break;
+          case 0: word="";break;
+          case 1: word="One";break;
+          case 2: word ="Two";break;
+          case 3: word ="Three";break;
+          case 4: word ="Four";break;
+          case 5: word="Five";break;
+          case 6:word="Six";break;
+          case 7:word="Seven";break;
+          case 8:word ="Eight";break;
+          case 9:word="Nine";break;
         }
-        System.out.print(word+" ");
+       if(n.length()-i==10){
+         System.out.print(word+" Billion ");
+         continue;}
+       if(n.length()-i==7){
+         if(n.length()>8){
+            if(n.charAt(i-1)!='0'&&n.charAt(i-2)!='0'){
+              System.out.print(word+" Million ");
+              continue;
+            }
+            else{ 
+              System.out.print(word+"");
+              continue;
+          }
+          }
+         System.out.print(word+" Million ");
+         continue;}
+       if(n.length()-i==4){
+         if(n.length()>5){
+            if(n.charAt(i-1)!='0'&&n.charAt(i-2)!='0'){
+              System.out.print(word+" Thousand ");
+              continue;
+            }
+            else{ 
+              System.out.print(word+"");
+              continue;
+          }
+          }
+         System.out.print(word+" Thousand ");
+         continue;}
+        System.out.print(word+"");
       }
-      if(n.length()-i==4 && Teen==true){
+      if((n.length()-i)%3==1 && Teen==true){
         switch(digit){
           case 0: word="Ten";
           break;
@@ -90,100 +120,46 @@ class Main {
           case 9:word="Nineteen";break;
         }
         Teen=false;
-        System.out.print(word+"");
-      }
-      
-      if(n.length()-i==3){
-        switch(digit){
-          case 0:word="";
-          break;
-          case 1: word="one hundred ";
-          break;
-          case 2: word="Two hundred ";
-          break;
-          case 3: word ="Three hundred ";
-          break;
-          case 4: word ="Four Hundred ";
-          break;
-          case 5:word ="Five Hundred ";
-          break;
-          case 6: word ="Six Hundred ";
-          break;
-          case 7: word ="Seven Hundred ";
-          break;
-          case 8: word="Eight Hundred ";
-          break;
-          case 9: word ="Nine Hundred ";
-          break;
+        if(n.length()-i==10){
+         System.out.print(word+" Billion ");
+         continue;}
+        if(n.length()-i==7){
+          if(n.length()>8){
+            if(n.charAt(i-1)!='0'&&n.charAt(i-2)!='0'){
+              System.out.print(word+" Million ");
+              continue;
+            }
+            else{ 
+              System.out.print(word+"");
+              continue;
+          }
+          }
+          System.out.print(word+" Million ");
+          continue;
         }
-        System.out.print(word+ "");
-      }
-      if(n.length()-i==2){
-        switch(digit){
-          case 0:word= "";
-          break;
-          case 1: Teen =true;//different sounds for words if 1 is in the tens place (Teens)
-          word="";
-          break;
-          case 2: word="Twenty ";
-          break;
-          case 3: word="Thirty ";
-          break;
-          case 4: word= "Forty ";
-          break;
-          case 5: word="Fifty ";
-          break;
-          case 6: word="Sixty ";
-          break;
-          case 7:word ="Seventy ";
-          break;
-          case 8:word="Eighty ";
-          break;
-          case 9:word="Ninety ";
-          break;
+        if(n.length()-i==4){
+          if(n.length()>5){
+            if(n.charAt(i-1)!='0'&&n.charAt(i-2)!='0'){
+              System.out.print(word+" Thousand ");
+              continue;
+            }
+            else{ 
+              System.out.print(word+"");
+              continue;
+          }
+          }
+          System.out.print(word+" Thousand ");
+          continue;
         }
-        System.out.print(word+"");
-      }
-      if(n.length()-i==1 &&Teen ==false){
-        switch(digit){
-          case 0: word="";break;
-          case 1: word="One";break;
-          case 2: word ="Two";break;
-          case 3: word ="Three";break;
-          case 4: word ="Four";break;
-          case 5: word="Five";break;
-          case 6:word="Six";break;
-          case 7:word="Seven";break;
-          case 8:word ="Eight";break;
-          case 9:word="Nine";break;
-        }
-        System.out.print(word+"");
-      }
-      if(n.length()-i==1 &&Teen==true){
-        switch(digit){
-          case 0: word="Ten";
-          break;
-          case 1:word ="Eleven";
-          break;
-          case 2:word="Twelve";break;
-          case 3:word ="Thirteen";break;
-          case 4: word="Fourteen";break;
-          case 5:word="Fifteen";break;
-          case 6:word="Sixteen";break;
-          case 7:word="Seventeen";break;
-          case 8:word="Eighteen";break;
-          case 9:word="Nineteen";break;
-        }
-        Teen =false;
-        System.out.print(word+"");
+         System.out.print(word+"");
       }
     }
   }
   public static void main(String[] args) {
-    int n;
+    long n;
     for(int i=0;i<args.length;i++){
-      n= Integer.parseInt(args[i]);
-      convert(n);
+      n= Long.parseLong(args[i]);
+      converter(n);
       System.out.print("\n");
     }
   }
